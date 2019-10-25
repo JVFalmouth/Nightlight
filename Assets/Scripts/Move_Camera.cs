51b90858651db18f54a4cb4508d51ab0849f6137
JamesV <jvanstone.work@gmail.com>
2019-10-25 03:12:43 +0100

    Added a script on the owl and bat prefabs to make them face their direction of movement.

--- Assets/Scripts/Move_Camera.cs
+++ Assets/Scripts/Move_Camera.cs
@@ -22,5 +22,4 @@ public class Move_Camera : MonoBehaviour
             }
         }
     }
-
 }
---
+++ Assets/Scripts/direction_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class direction_script : MonoBehaviour
+{
+    Transform parentTransform;
+    SpriteRenderer spriteRenderer;
+    private float objectX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentTransform = gameObject.GetComponentInParent<Transform>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        objectX = parentTransform.position.x;
+    }
+
+    void Update()
+    {
+        if (objectX > parentTransform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (objectX < parentTransform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        objectX = parentTransform.position.x;
+    }
+}
